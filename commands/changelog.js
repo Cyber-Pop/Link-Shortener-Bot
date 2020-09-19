94d9e553c700a87b08ebe6e2a455577195719185
TheLimifiedCode <thelimifiedlime@protonmail.com>
2020-09-19 00:29:07 +0000

    Changed changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -8,7 +8,9 @@ module.exports = {
     let embed = {
       color: 0x9900ff,
       title: `Version 2.0`,
-      description: `• Added changelog command\n• Made code less messy\n• Bot tells you correct usage if arguments are missing\n• Added v.gd as a link shortner (${config.prefix}vgd [link])\n• New ping command (It's useless but every bot ***must*** have one)\n\nThat's it folks stay tuned for the next update`, 
+      description: `• Added a !stats command
+      
+                    That's it folks stay tuned for the next update`, 
       author: { 
         name: `Changelog`, 
         icon_url: avatar
