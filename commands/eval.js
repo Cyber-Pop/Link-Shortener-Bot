e6f4820b3c0d9f74381453f226bab1a22a897db1
TheLimifiedCode <thelimifiedlime@protonmail.com>
2020-10-03 04:16:06 +0000

    Fixed some Github issues

---
+++ commands/eval.js
@@ -0,0 +1,27 @@
+module.exports = {
+  ownerOnly: true,
+  name: 'eval',
+  cooldown: 3,
+  args: false,
+  execute(msg, args, client, strings) {
+    console.log(`Executed eval by ${msg.author.username}`)
+    let avatar = client.user.displayAvatarURL();
+    
+    try {
+      const code = args.join(' ')
+      eval(code)
+      
+    } catch (e) {
+        let embed = {
+          color: strings.errorColor,
+          description: `There was an error while running the code. The error has been included below\n \`\`\`${e}\`\`\`\   `, 
+          author: { 
+            name: `Error`, 
+            icon_url: avatar
+          }
+        }
+
+        msg.channel.send({ embed: embed })
+    }
+  }
+}
