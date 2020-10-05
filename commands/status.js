d3192a3e510c991ea9ef0800479df834453d34f9
TheLimifiedCode <thelimifiedlime@protonmail.com>
2020-10-05 01:09:31 +0000

    Status mostly working

--- commands/status.js
+++ commands/status.js
@@ -7,15 +7,42 @@ module.exports = {
     const joined = args.join('')
     const split = joined.trim().split('|')
     // NOT DONE NEED TO CHECK IF ARGUMENTS REQUIRE FOR VARIABLES ARE EMPHTY
-    let name = (split[0].length) ? split[0] : `${strings.prefix}help`;
-    let activity;
-    let status
-    console.log(split)
+    let name = (split[0]) ? split[0] : `${strings.prefix}help`;
+    let activity = (split[1]) ? split[1].toUpperCase() : `LISTENING`;
+    let status = (split[2]) ? split[2].toUpperCase() : `ONLINE`;
 
-    let final = { activity: { name: `${name}`, type: `LISTENING` }, status: `online` };
+    console.log(split)
+    console.log(`${activity} is ${activity.length} characters long!`)
+
+    if (activity === `PLAYING`) {
+      activity = `PLAYING`
+    } else if (activity === `STREAMING`) {
+      activity = `STREAMING`
+    } else if (activity === `WATCHING`) {
+      activity = `WATCHING`
+    } else {
+      activity = `LISTENING`
+    }
+
+    console.log(status)
+
+    if (status === `ONLINE`) {
+      status = `online`
+    } else if (status === `IDLE`) {
+      status = `idle`
+    } else if (status === `DND`) {
+      status = `dnd`
+      console.log(`dnd?`)
+    } else {
+      status = `ONLINE`
+    }
+
+    console.log(status)
+
+    let final = { activity: { name: `${name}`, type: `${activity}` }, status: `${status}` };
 
     client.user.setPresence(final)
-    .then(console.log(`Set status`))
+    .then(msg.channel.send(`Updated Status`))
     .catch(console.error);
   }
 }
