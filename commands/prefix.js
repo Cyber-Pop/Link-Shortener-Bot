0f291eedd376cefc0c33df3c8f154f4f732f6f50
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-10-16 03:03:46 +0000

    Added a untested check to see if a user is a administrator

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,7 +5,9 @@ module.exports = {
   execute(msg, args, client, strings, prefixes) {
     async function setPrefix() {
       console.log(args)
+      if (!msg.author.permissions.has('ADMINISTRATOR')) return msg.channel.send(`You need to be an administrator to change the prefix`)
       await prefixes.set(msg.guild.id, args[0])
+      msg.channel.send(`The prefix has been updated to \`${args[0]}\` `)
     }
 
     setPrefix()
