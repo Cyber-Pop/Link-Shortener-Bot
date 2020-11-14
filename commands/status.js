3dd9e13c6cb1d7da0c7ea43510704107e7726eb9
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-11-14 20:42:20 +0000

    semi working command

--- commands/status.js
+++ commands/status.js
@@ -5,45 +5,18 @@ module.exports = {
   cooldown: 3,
   usage: '<name> <activity>',
  execute(msg, args, client, config, prefix, axios, Discord, avatar) {
-    const joined = args.join('')
-    const split = joined.trim().split('|')
-    // NOT DONE NEED TO CHECK IF ARGUMENTS REQUIRE FOR VARIABLES ARE EMPHTY
-    let name = (split[0]) ? split[0] : `${strings.prefix}help`;
-    let activity = (split[1]) ? split[1].toUpperCase() : `LISTENING`;
-    let status = (split[2]) ? split[2].toUpperCase() : `ONLINE`;
+    const joined = args.join(' ')
+    const split = joined.split('|')
 
-    console.log(split)
-    console.log(`${activity} is ${activity.length} characters long!`)
+    const presence = split[0].trim()
+    const type = split[1].trim()
+    const status = split[2].trim()
+    console.log(`"${presence}" `)
+    console.log(`"${type}" `)
+    console.log(`"${status}" `)
 
-    if (activity === `PLAYING`) {
-      activity = `PLAYING`
-    } else if (activity === `STREAMING`) {
-      activity = `STREAMING`
-    } else if (activity === `WATCHING`) {
-      activity = `WATCHING`
-    } else {
-      activity = `LISTENING`
-    }
-
-    console.log(status)
-
-    if (status === `ONLINE`) {
-      status = `online`
-    } else if (status === `IDLE`) {
-      status = `idle`
-    } else if (status === `DND`) {
-      status = `dnd`
-      console.log(`dnd?`)
-    } else {
-      status = `ONLINE`
-    }
-
-    console.log(status)
-
-    let final = { activity: { name: `${name}`, type: `${activity}` }, status: `${status}` };
-
-    client.user.setPresence(final)
-    .then(msg.channel.send(`Updated Status`))
-    .catch(console.error);
+    client.user.setPresence({activity: {name: presence}, status: status})
+    client.user.setActivity(type)
+    msg.channel.send(`Updated!`)
   }
 }
