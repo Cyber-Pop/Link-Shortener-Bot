d3ef940b9e28315347baa405c3ddc3758f8d97af
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-11-14 20:54:07 +0000

    WORKING STATUS COMMAND

--- commands/status.js
+++ commands/status.js
@@ -9,14 +9,11 @@ module.exports = {
     const split = joined.split('|')
 
     const presence = split[0].trim()
-    const type = split[1].trim()
-    const status = split[2].trim()
+    const status = split[1].trim()
     console.log(`"${presence}" `)
-    console.log(`"${type}" `)
     console.log(`"${status}" `)
 
     client.user.setPresence({activity: {name: presence}, status: status})
-    client.user.setActivity(type)
     msg.channel.send(`Updated!`)
   }
 }
