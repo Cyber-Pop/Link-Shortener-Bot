676de60e9ded032e3a9429235f33970f227ad354
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-11-28 21:40:20 +0000

    Reverted 8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,7 +7,6 @@ module.exports = {
   usage: '<question>',
   cooldown: 3,
   execute(msg, args, client, config, prefix, axios, Discord, avatar) {
-    throw 'arctic gae'
     let items = ['Yes','No', 'For certain', 'As I see it, yes', 'Ask again later', 'Concentrate and ask again', 'You have been blacklisted', 'Cannot predict now', 'Better not tell you know', 'Don\'t count on it', 'It is certain', 'It is decidedly so', 'Most likely','You have been granted administrator perms', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Outlook good', 'Signs point to yes', 'Without a doubt', 'You may rely on it']
     const min = 0
     const max = items.length - 1
