5f164c92ce144efe02ed69d3d5d98ef413691ef5
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-11-28 03:34:11 +0000

    Works now

--- functions/random-letters.js
+++ functions/random-letters.js
@@ -1,7 +1,7 @@
 module.exports = {
   random(length) {
     if (isNaN(length)) {
-     return  throw new TypeError(`length must be a number`)
+      throw new TypeError(`"length" should be a number`)
     }
     console.log('defnining letters')
     const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -9,7 +9,7 @@ module.exports = {
     let string = 'e'
 
     for (count = 0; count <= length; count++) {
-      const randomNum = Math.round(Math.random() * letters.length)
+      const randomNum = Math.floor(Math.random() * letters.length)
       string += letters[randomNum]
     }
 
