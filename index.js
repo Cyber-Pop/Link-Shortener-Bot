68a6f6a0d2698f60d9228e95e8b4b6f983c1e447
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-11-30 21:20:13 +0000

    Fixed undefined issue

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const Statcord = require("statcord.js");
 const token = process.env.BOT_TOKEN
 const statcordToken = process.env.STATCORD_TOKEN
 let statcord;
-const manager = new ShardingManager('./bot.js', { token: token});
+const manager = new ShardingManager('./bot.js', { token: token });
 const chalk = require('chalk')
 const config = require('./config.json');
 
 if (config.pingRequired) {
   const runner = require('./functions/express.js')
   runner.startExpress()
- }
+}
 
 if (statcordToken) {
   statcord = new Statcord.ShardingClient({
@@ -20,12 +20,12 @@ if (statcordToken) {
     postMemStatistics: true, /* Whether to post memory statistics or not, defaults to true */
     postNetworkStatistics: true, /* Whether to post memory statistics or not, defaults to true */
     autopost: true /* Whether to auto post or not, defaults to true */
-});
+  });
 }
 
 manager.on('shardCreate', shard => {
-  console.log(chalk.bgGreenBright(`SHARD`) ,`Launched Shard ${shard.id}`);
-  
+  console.log(chalk.bgGreenBright(`SHARD`), `Launched Shard ${shard.id}`);
+
   shard.on('death', shard => {
     console.log(chalk.bgRedBright(`SHARD`), `Shard Died`)
   })
@@ -39,14 +39,16 @@ manager.on('shardCreate', shard => {
 
 manager.spawn();
 
-statcord.on("autopost-start", () => {
+if (statcord) {
+  statcord.on("autopost-start", () => {
     // Emitted when statcord autopost starts
     console.log("Started autopost");
-});
+  });
 
-statcord.on("post", status => {
+  statcord.on("post", status => {
     // status = false if the post was successful
     // status = "Error message" or status = Error if there was an error
     if (!status) console.log("Successful post");
     else console.error(status);
-});
+  });
+}
