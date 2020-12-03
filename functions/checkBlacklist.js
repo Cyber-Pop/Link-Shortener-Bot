19d6a7b3875a2af4f7d243df52e1fdadc4ba40ec
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-12-03 22:19:45 +0000

    fix?

--- functions/checkBlacklist.js
+++ functions/checkBlacklist.js
@@ -1,14 +1,14 @@
 module.exports = {
-  check(msg, blacklist) {
-    async function run() {
+  async function check(msg, blacklist) {
+    //async function run() {
       let user = await blacklist.get(msg.author.id)
       let blacklisted
       if (user) {
         msg.channel.send(`Sorry but you have been blacklisted`);
-         blacklisted = await true;
+        blacklisted = await true;
       }
 
-    }
+    //}
     
     if (blacklisted) {
       console.log(`blacklisted`)
